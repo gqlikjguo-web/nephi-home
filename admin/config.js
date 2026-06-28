a8988291d3f0c55081c9002361daf07a5994f4fe
gqlikjguo-web <gqlikjguo@gmail.com>
2026-06-28 18:36:48 +0800

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -2,3 +2,4 @@
 // Example: const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfy.../exec";
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwdsAYER4n0vriCwRgMCpuve-mLBk3GtUco9GlTCR3tnRhXH08oM2ELk4m-YFPH8uHr/exec";
 const ADMIN_PASSWORD = "nephi2026";
+
