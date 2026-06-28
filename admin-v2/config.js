5b02f7434542995c6071a9712e4e3e2642970982
gqlikjguo-web <gqlikjguo@gmail.com>
2026-06-28 21:26:33 +0800

    Update config.js

--- admin-v2/config.js
+++ admin-v2/config.js
@@ -1,4 +1,4 @@
 window.NEPHI_ADMIN_V2_CONFIG = {
-  appsScriptUrl: "PASTE_NEW_APPS_SCRIPT_V2_EXEC_URL_HERE",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwdsAYER4n0vriCwRgMCpuve-mLBk3GtUco9GlTCR3tnRhXH08oM2ELk4m-YFPH8uHr/exec",
   adminPassword: "nephi2026"
 };
